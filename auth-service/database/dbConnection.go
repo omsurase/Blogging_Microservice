00d39c0f975c9928d2d18727a298c5b146ac1e4a
omsurase <omsurase2129@gmail.com>
2024-08-01 05:13:55 +0530

    db connection

--- auth-service/database/dbConnection.go
+++ auth-service/database/dbConnection.go
@@ -1,1 +1,59 @@
-package database
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
+)
+
+func DBinstance() *mongo.Client {
+
+	// Set client options
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MongoDB := os.Getenv("MONGODB_URL")
+	clientOptions := options.Client().ApplyURI(MongoDB)
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	// Check the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	// Disconnect
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
+}
