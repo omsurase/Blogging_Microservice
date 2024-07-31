a070a602a6a53c0b1de80043eddef8f0852f4eb7
omsurase <omsurase2129@gmail.com>
2024-08-01 04:09:06 +0530

    Added user routes.

--- auth-service/routes/authRouter.go
+++ auth-service/routes/authRouter.go
@@ -2,8 +2,10 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	controller "github.com/omsurase/Blogging_Microservice/auth-service/controllers"
 )
 
-func AuthRoutes(router *gin.Engine) {
-
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.SignUp())
+	incomingRoutes.POST("users/login", controller.Login())
 }
